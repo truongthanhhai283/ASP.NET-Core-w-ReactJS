c564da9f728e2e31f045826fdb7ff98540c768b0
TruongThanhHai28 <truongthanhhai283@gmail.com>
2021-04-21 14:35:13 +0700

    3--Adding a Details handler

--- MyApp/API/Controllers/ActivitiesController.cs
+++ MyApp/API/Controllers/ActivitiesController.cs
@@ -21,7 +21,7 @@ namespace API.Controllers
         [HttpGet("{id}")] //activities/id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
